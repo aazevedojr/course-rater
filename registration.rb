91b8d4c25bc1162bb769e18a791e8f33d058f43e
Augusto Azevedo <augusto.azevedo.jr@gmail.com>
2017-06-11 20:27:04 -0700

    Fix method argument error

--- registration.rb
+++ registration.rb
@@ -19,15 +19,17 @@ end
 def new_maker(creator_name)
   @db.execute("INSERT INTO makers (name)
     VALUES (?)", [creator_name])
-  @db.execute("SELECT LAST_INSERT_ROWID();")[0]
+  @db.execute("SELECT LAST_INSERT_ROWID();")[0][0]
 end
 
 def new_course(course_title, code, maker_ID)
   @db.execute("INSERT INTO courses (name, code, maker_id)
     VALUES (?, ?, ?)", [course_title, code, maker_ID])
+  @db.execute("SELECT LAST_INSERT_ROWID();")[0][0]
 end
 
 # User interface
+
 
 puts "Enter the 19 digit code that is your course ID (e.g. '2D91-0000-02D6-F1E9')."
 
@@ -38,6 +40,6 @@ doc = Nokogiri::HTML(open(to_url(input)))
 creator_name = doc.css('.creator-info .name')[0].content
 course_title = doc.css('.course-title')[0].content
 
-maker_ID = new_maker(input)
+maker_ID = new_maker(creator_name)
 
 new_course("test course", input, maker_ID)
