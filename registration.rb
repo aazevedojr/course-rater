5582ae7f9596010156f5e8d4a581601429801b52
Augusto Azevedo <augusto.azevedo.jr@gmail.com>
2017-06-11 18:49:16 -0700

    Fetch data from website to register user and course

---
+++ registration.rb
@@ -0,0 +1,43 @@
+# Entering the challenge:
+
+require_relative 'setup'
+require 'nokogiri'
+require 'open-uri'
+
+require 'openssl'
+OpenSSL::SSL::VERIFY_PEER = OpenSSL::SSL::VERIFY_NONE
+
+def to_url(code)
+  "https://supermariomakerbookmark.nintendo.net/courses/" + code
+end
+
+
+def register(course_code)
+  # retrieves data using nokogiri
+end
+
+def new_maker(creator_name)
+  @db.execute("INSERT INTO makers (name)
+    VALUES (?)", [creator_name])
+  @db.execute("SELECT LAST_INSERT_ROWID();")[0]
+end
+
+def new_course(course_title, code, maker_ID)
+  @db.execute("INSERT INTO courses (name, code, maker_id)
+    VALUES (?, ?, ?)", [course_title, code, maker_ID])
+end
+
+# User interface
+
+puts "Enter the 19 digit code that is your course ID (e.g. '2D91-0000-02D6-F1E9')."
+
+input = gets.chomp
+
+doc = Nokogiri::HTML(open(to_url(input)))
+
+creator_name = doc.css('.creator-info .name')[0].content
+course_title = doc.css('.course-title')[0].content
+
+maker_ID = new_maker(input)
+
+new_course("test course", input, maker_ID)
