51c6b7556c668a9e1b7ec2fd8e3009bf3b6b788e
Augusto Azevedo <augusto.azevedo.jr@gmail.com>
2017-07-08 16:55:05 -0700

    It's alive

---
+++ csv2db.rb
@@ -0,0 +1,51 @@
+require 'csv'
+require_relative 'registration'
+require_relative 'new_challenge'
+
+CIDs = [
+  'A808-0000-033C-C953',
+  'EA2F-0000-033D-625C',
+  '6684-0000-033D-AB0F',
+  'EDB2-0000-033E-52D2',
+  '4272-0000-033E-668D',
+  '6B53-0000-033F-0B33',
+  '96DD-0000-0340-3722',
+  'D6CE-0000-0340-3F5D',
+  '59F9-0000-0341-CFC9'
+]
+
+
+PIDs = [
+  "RZudomon",
+  "lukengl",
+  "WIIUREOLI79",
+  "adsosabado",
+  "Bibibombinha",
+  "jogo_jogos",
+  "brayer.souza"
+]
+
+def csv2db(filename)
+  row_no = 0
+  CSV.foreach(filename, headers: true, header_converters: :symbol) do |row|
+    col_no = 2
+    pid = PIDs[row_no]
+    CIDs.each do |cid|
+      new_rating(row[col_no].to_i, row[col_no + 1], pid, cid)
+      col_no += 2
+    end
+    row_no += 1
+  end
+end
+
+# "xTorviCx"
+# "Stopinski"
+
+
+challenge_ID = new_challenge('Super Heroes', nil, nil)
+
+CIDs.each do |cid|
+  add_maker_n_course_to_db(cid, challenge_ID)
+end
+
+csv2db('super_heroi.csv')
--- registration.rb
+++ registration.rb
@@ -11,29 +11,37 @@ def to_url(code)
   "https://supermariomakerbookmark.nintendo.net/courses/" + code
 end
 
-def new_maker(creator_name)
-  @db.execute("INSERT INTO makers (name)
-    VALUES (?)", [creator_name])
+def new_maker(id, name)
+  @db.execute("INSERT OR REPLACE INTO makers (id, name)
+    VALUES (?, ?)", [id, name])
+  id
+end
+
+def new_course(id, title, maker_id, challenge_id)
+  @db.execute("INSERT OR REPLACE INTO courses (id, title, maker_id, challenge_id)
+    VALUES (?, ?, ?, ?)", [id, title, maker_id, challenge_id])
+  id
+end
+
+def new_rating(rating, feedback, maker_id, course_id)
+  @db.execute("INSERT INTO reviews (rating, feedback, maker_id, course_id)
+    VALUES (?, ?, ?, ?)", [rating, feedback, maker_id, course_id])
   @db.execute("SELECT LAST_INSERT_ROWID();")[0][0]
 end
 
-def new_course(course_title, code, maker_ID)
-  @db.execute("INSERT INTO courses (name, code, maker_id)
-    VALUES (?, ?, ?)", [course_title, code, maker_ID])
-  @db.execute("SELECT LAST_INSERT_ROWID();")[0][0]
+
+def add_maker_n_course_to_db(course_id, challenge_id)
+  doc = Nokogiri::HTML(open(to_url(course_id)))
+  creator_id = /\/profile\/(.*)\?/.match(doc.css('.course-detail-wrapper a#mii')[0].attributes['href'].value)[1]
+  creator_name = doc.css('.creator-info .name')[0].content
+  course_title = doc.css('.course-title')[0].content
+  maker_id = new_maker(creator_id, creator_name)
+  new_course(course_id, course_title, maker_id, challenge_id)
 end
+
 
 # User interface
 
-puts "Enter the 19 digit code that is your course ID (e.g. '2D91-0000-02D6-F1E9')."
+# puts "Enter the 19 digit code that is your course ID (e.g. '2D91-0000-02D6-F1E9')."
 
-input = gets.chomp
-
-doc = Nokogiri::HTML(open(to_url(input)))
-
-creator_name = doc.css('.creator-info .name')[0].content
-course_title = doc.css('.course-title')[0].content
-
-maker_ID = new_maker(creator_name)
-
-new_course("test course", input, maker_ID)
+# input = gets.chomp
